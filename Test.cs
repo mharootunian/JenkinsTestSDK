91e294bbed97de0b4998adca1a7ff4d061d63da5
Meero Harootunian <meero.harootunian@gmail.com>
2023-04-24 19:52:00 -0700

    first

---
+++ Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Login;
+using Core.TestExecution;
+
+namespace JenkinsTestSuite {
+    public class Test : Check {
+        public override void BeforeExecute() {
+            PxLogin.LogIn();
+        }
+
+        public override void Execute() {
+            Test1();
+        }
+
+        private void Test1() {
+
+        }
+    }
+}
